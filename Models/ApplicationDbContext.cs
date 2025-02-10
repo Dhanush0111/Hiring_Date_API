365f76ca6bb6c06977ba8ee6b1c983e618bf5dcb
Dhanush0111 <dhanushms077@gmail.com>
2025-02-10 16:29:12 +0530

    Added Context file

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -6,7 +6,37 @@ namespace Hiring_Date_API.Models
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+
         }
-        
+
+        public DbSet<Hiring> Hirings { get; set; }
+        public DbSet<Company> Companys { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.HasKey(c => c.CompanyId);
+
+                entity.Property(c => c.CompanyName).IsRequired().HasMaxLength(200);
+
+            });
+
+            modelBuilder.Entity<Hiring>(entity => {
+                
+                entity.HasKey(h => h.HiringId);
+
+                entity.Property(h => h.CompanyName).IsRequired().HasMaxLength(200);
+
+                entity.Property(h => h.Activity).IsRequired().HasMaxLength(200);
+
+                entity.HasOne(h => h.Company_CompanyId).WithOne().HasForeignKey<Company>(c => c.CompanyId);
+
+
+            
+            
+            });
+        }
     }
 }

03ba33941f33612f9342f826b4eeeb43b17275ea
Dhanush0111 <dhanushms077@gmail.com>
2025-02-10 16:43:58 +0530

    Added CompanyController

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Hiring_Date_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hiring_Date_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CompanyController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]   
+        public async Task<List<Company>> getcompany()
+        {
+            return await _context.Companys.ToListAsync();
+        }
+
+
+    }
+}

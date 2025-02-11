69ae616dce1b7b5664c824f91e91cd29a4bf8167
adii-b <adityabharadwaj273@gmail.com>
2025-02-11 19:52:10 +0530

    Merge branch by aditya

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -17,13 +17,61 @@ namespace Hiring_Date_API.Controllers
         [HttpGet]
         public async Task<List<Company>> GetCompany()
         {
-            return await _context.Companys.ToListAsync();
+            return await _context.Companies.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<Company> GetByCompanyId(int id)
+
+        public async Task<IActionResult> Getbyid(int? id)
         {
-            return await _context.Companys.FindAsync(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(await _context.Companies.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Company>> CompanyPostDetails(Company company)
+        {
+            if (company == null)
+            {
+                return BadRequest("Invalid company detail");
+            }
+            await _context.Companies.AddAsync(company);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Getbyid), new { id = company.CompanyId }, company);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Updating(int id, Company company)
+        {
+            if (id != company.CompanyId)
+            {
+                return BadRequest("Invalid Input");
+            }
+            var user = await _context.Companies.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.CompanyName = company.CompanyName;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.Companies.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _context.Companies.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
